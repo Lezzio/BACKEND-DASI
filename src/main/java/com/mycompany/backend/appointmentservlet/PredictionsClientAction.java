eeee88b77344da6308d5107033c5e12e6fc762c6
Allan Guigal <allan.guigal0@gmail.com>
2021-05-20 04:23:49 +0200

    Update prediction action for production

--- src/main/java/com/mycompany/backend/appointmentservlet/PredictionsClientAction.java
+++ src/main/java/com/mycompany/backend/appointmentservlet/PredictionsClientAction.java
@@ -6,50 +6,60 @@
 package com.mycompany.backend.appointmentservlet;
 
 import com.mycompany.backend.Action;
+import com.mycompany.td2.dasi.metier.modele.AstralProfile;
+import com.mycompany.td2.dasi.metier.modele.Client;
+import com.mycompany.td2.dasi.metier.modele.Consultation;
+import com.mycompany.td2.dasi.metier.modele.Employee;
+import com.mycompany.td2.dasi.metier.services.AppointmentService;
 import com.mycompany.td2.dasi.metier.services.EntityService;
 import com.mycompany.td2.dasi.utils.AstroNetApi;
 import java.io.IOException;
-import static java.lang.Integer.parseInt;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
-/**
- *
- * @author 
- */
-public class PredictionsClientAction extends Action{
-
+public class PredictionsClientAction extends Action {
+    
    @Override
     public void executer(HttpServletRequest request){
         String love = request.getParameter("loveScore");
         String health = request.getParameter("healthScore");
         String work = request.getParameter("workScore");
-        String color = request.getParameter("colorHappiness");
-        String animal = request.getParameter("animalHappiness");
-        int loveScore = parseInt(love);
-        int healthScore = parseInt(health);
-        int workScore = parseInt(work);
-
-
-        // Appeler la méthode de génération des prédicitions
-
-
-        EntityService entityService = new EntityService();       
+        
+        //Scores asked by the employee
+        Integer healthScore = Integer.parseInt(health);
+        Integer workScore = Integer.parseInt(work);
+        Integer loveScore = Integer.parseInt(love);
+        
+        //Fields related to the astral profile of the client in live consultation
+        HttpSession session = request.getSession();
+        Long employeeId = (Long) session.getAttribute("employeeId");
+        
+        AppointmentService appointmentService = new AppointmentService();
+        EntityService entityService = new EntityService();
+        
+        Employee employee = entityService.searchEmployeeById(employeeId);
+        Consultation consultation = appointmentService.getEmployeeActiveConsultation(employee);
+        
+        Client client = consultation.getClient();
+        AstralProfile astralProfile = client.getAstralProfile();
+        
+        String color = astralProfile.getColor();
+        String animal = astralProfile.getTotemAnimal();
+        
+        //Call the astronet api to generate the predictions
         AstroNetApi astroNet = new AstroNetApi();
-
-        List<String> predictions = new ArrayList<String>();
        try {
-           predictions = astroNet.getPredictions(color, animal, loveScore, healthScore, workScore);
-           // Renvoi des prédictions dans la request
-            request.setAttribute("lovePred", predictions.get(0) );
-            request.setAttribute("healthPred", predictions.get(1));
-            request.setAttribute("workPred", predictions.get(2));
+           List<String> predictions = astroNet.getPredictions(color, animal, loveScore, healthScore, workScore);
+           //Set predictions in the request attributes
+           request.setAttribute("lovePred", predictions.get(0) );
+           request.setAttribute("healthPred", predictions.get(1));
+           request.setAttribute("workPred", predictions.get(2));
        } catch (IOException ex) {
            Logger.getLogger(PredictionsClientAction.class.getName()).log(Level.SEVERE, null, ex);
-           System.out.println("Erreur lors de l'appel de la méthode de prédictions ");
+           System.out.println("Error while calling the prediction action ");
        }
     }
 
--- src/main/java/com/mycompany/backend/appointmentservlet/PredictionsClientSerialisation.java
+++ src/main/java/com/mycompany/backend/appointmentservlet/PredictionsClientSerialisation.java
@@ -14,41 +14,32 @@ import java.io.PrintWriter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author victo
- */
 public class PredictionsClientSerialisation extends Serialisation {
 
-    private final Gson gson = new Gson();
+    Gson gson = new GsonBuilder().setPrettyPrinting().serializeNulls().create();
+    
     @Override
     public void serialiser(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
     // Lecture des attributs de la requête (stockés par action)
-    String lovePred = (String)request.getAttribute("lovePred");
-    String healthPred = (String)request.getAttribute("healthPred");
-    String workPred = (String)request.getAttribute("workPred");
+    String lovePred = (String) request.getAttribute("lovePred");
+    String healthPred = (String) request.getAttribute("healthPred");
+    String workPred = (String) request.getAttribute("workPred");
 
-
+    // Ajout de propriétés au conteneur JSON
     JsonObject container = new JsonObject();
-    if(lovePred != null)
-    {   
-        // Ajout de propriétés au conteneur JSON
+    if(lovePred != null) {   
         container.addProperty("love", lovePred);    
     }
-    if(healthPred != null)
-    {
+    if(healthPred != null) {
         container.addProperty("health", healthPred);
     }
-    if(workPred != null)
-    {
+    if(workPred != null) {
         container.addProperty("work", workPred);
     }
 
-
     // Formatage de la structure de données JSON => Ecriture dur le flux de sortie de la réponse
     PrintWriter out = response.getWriter();
-    Gson gson = new GsonBuilder().setPrettyPrinting().serializeNulls().create();
     gson.toJson(container, out);
     out.close();
     }
